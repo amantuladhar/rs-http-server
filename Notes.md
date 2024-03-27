e65b49177b0f39bc643563835e69bd03e84ef2d2
Aman Tuladhar <atuladhar.aman@gmail.com>
2024-03-26 21:14:10 -0500

    Send HttpStatus Ok code

---
+++ Notes.md
@@ -0,0 +1,6 @@
+## Step 1
+- You can run your program using `cargo run`
+- To test your initial program you can use `nc -vz 127.0.0.1 4221`
+
+
+## Step 2
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,12 +9,26 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                let msg = gen_http_response(200);
+                _stream.write(msg.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn gen_http_response(status: u16) -> String {
+    let status_line = get_status_line(status);
+    format!("HTTP/1.1 {status_line}\r\n\r\n")
+}
+
+fn get_status_line<'a>(status: u16) -> &'a str {
+    match status {
+        200 => "200 OK",
+        404 => "404 Not Found",
+        _ => "500 Internal Server Error",
     }
 }
