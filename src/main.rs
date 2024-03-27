dd82d923907edd9578a90dbce5892bccc93769aa
seg4lt <12027886+seg4lt@users.noreply.github.com>
2024-03-26 21:41:13 -0500

    Read start line and find path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,8 +13,20 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let msg = gen_http_response(200);
-                _stream.write(msg.as_bytes()).unwrap();
+                let mut reader = BufReader::new(&_stream);
+                let mut request_str = String::new();
+                // Just read one line for now
+                reader.read_line(&mut request_str).unwrap();
+                let start_line = request_str.split(" ").collect::<Vec<&str>>()[1];
+
+                let status_code = match start_line {
+                    "/" => 200,
+                    _ => 404,
+                };
+                let msg = gen_http_response(status_code);
+                if let Err(err) = _stream.write(msg.as_bytes()) {
+                    println!("Error occurred while sending data: {}", err);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
