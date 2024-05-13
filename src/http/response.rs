f67b870d64691935d8cc04b11dfaf5070e3df82c
Aman Tuladhar <atuladhar.aman@gmail.com>
2024-05-12 19:23:08 -0500

    Compress content

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,15 +1,19 @@
+use std::io::Write;
+
+use flate2::write::GzEncoder;
+
 use crate::HTTP_LINE_ENDING;
 
 use super::{
-    accept_encoding::AcceptEncoding, content_type::ContentType, header::Header,
-    status_code::StatusCode,
+    content_type::ContentType, encoding::Encoding, header::Header, status_code::StatusCode,
 };
 
 pub struct Response {
     body: Option<Vec<u8>>,
     status_code: StatusCode,
     content_type: ContentType,
-    accept_encoding: Option<AcceptEncoding>,
+    accept_encoding: Option<Encoding>,
+    content_length: Option<usize>,
 }
 
 impl Response {
@@ -26,31 +30,27 @@ impl Response {
             Header::ContentType.to_str(),
             self.content_type.to_str()
         ));
-        let _ = self.accept_encoding.as_ref().and_then(|accept_encoding| {
+        if let Some(accept_encoding) = &self.accept_encoding {
             msg_lines.push(format!(
                 "{}: {}",
                 Header::ContentEncoding.to_str(),
                 accept_encoding.to_str()
             ));
-            Some(())
-        });
-        match &self.body {
-            Some(body) => msg_lines.push(format!(
+        }
+        if let Some(content_length) = &self.content_length {
+            msg_lines.push(format!(
                 "{}: {}",
                 Header::ContentLength.to_str(),
-                body.len()
-            )),
-            _ => msg_lines.push("".to_string()),
-        };
-
+                content_length
+            ));
+        }
         msg_lines.push(HTTP_LINE_ENDING.to_string());
         // Body
-        let mut bytes = msg_lines.join(HTTP_LINE_ENDING).as_bytes().to_vec();
-        match &self.body {
-            Some(body) => bytes.extend(body),
-            _ => {}
-        };
-        bytes
+        let mut response_bytes = msg_lines.join(HTTP_LINE_ENDING).as_bytes().to_vec();
+        if let Some(body) = &self.body {
+            response_bytes.extend(body);
+        }
+        response_bytes
     }
 }
 
@@ -58,7 +58,8 @@ pub struct ResponseBuilder {
     status_code: StatusCode,
     body: Option<Vec<u8>>,
     content_type: ContentType,
-    accept_encoding: Option<AcceptEncoding>,
+    accept_encoding: Option<Encoding>,
+    content_length: Option<usize>,
 }
 
 impl ResponseBuilder {
@@ -68,9 +69,10 @@ impl ResponseBuilder {
             body: None,
             content_type: ContentType::Plain,
             accept_encoding: None,
+            content_length: None,
         }
     }
-    pub fn accept_encoding(mut self, accept_encoding: Option<AcceptEncoding>) -> Self {
+    pub fn accept_encoding(mut self, accept_encoding: Option<Encoding>) -> Self {
         self.accept_encoding = accept_encoding;
         self
     }
@@ -80,16 +82,33 @@ impl ResponseBuilder {
     }
     pub fn body(mut self, body: Option<Vec<u8>>) -> Self {
         self.body = body;
+        if let Some(body) = &self.body {
+            self.content_length = Some(body.len());
+        }
         self
     }
     pub fn content_type(mut self, content_type: ContentType) -> Self {
         self.content_type = content_type;
         self
     }
-    pub fn build(&self) -> Response {
+    pub fn build(&mut self) -> Response {
+        if let Some(body) = &self.body {
+            if let Some(accept_encoding) = &self.accept_encoding {
+                match accept_encoding {
+                    Encoding::Gzip => {
+                        let mut encoder =
+                            GzEncoder::new(Vec::new(), flate2::Compression::default());
+                        encoder.write_all(body).expect("unable to compress body");
+                        self.body = Some(encoder.finish().expect("unable to finish compression"));
+                        self.content_length = Some(self.body.as_ref().unwrap().len());
+                    }
+                }
+            }
+        }
         Response {
             status_code: self.status_code,
             body: self.body.clone(),
+            content_length: self.content_length,
             content_type: self.content_type,
             accept_encoding: self.accept_encoding.clone(),
         }
